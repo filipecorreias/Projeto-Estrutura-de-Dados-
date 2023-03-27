8befc2902243c73e473f71542986be5e9993da9a
Filipe Correia <105663557+filipecorreias@users.noreply.github.com>
2023-03-27 11:50:20 +0100

    Update MenuCliente.c

--- MenuCliente.c
+++ MenuCliente.c
@@ -1,17 +1,68 @@
 //-Menu Inicial
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "cliente.h"
 
 clientes* pessoas = NULL;
+automoveis* mobilidade = NULL;
+
 
 
 void EscolhasClientes() {
+	int mobilidadeescolhida, auxcod;
+	float novosaldo = 0;
+	
+	auxcod = pessoas->id;
+	//printf("%d\n", auxcod);
+
+	if (mobilidade == NULL) {
+		mobilidade = LerMobilidade();
+	}
+
+
+	printf("Bem vindo %s, seu saldo atual e de: %.2f\n", pessoas->nome, pessoas->saldo);
+	printf("\n");
+	printf("O que deseja fazer?\n");
+	int escolha;
 	printf("1- Alugar Mobilidade\n");
 	printf("2- Adicionar Saldo\n");
 	printf("3- Ver cidades disponiveis\n");
 	printf("4- Exit\n");
+	scanf("%d", &escolha);
+
+	switch (escolha)
+	{
+	case 1:
+		system("cls");
+		ListarMobilidadeAluguel(mobilidade);
+		printf("Digite o numero da mobilidade que deseja alugar: ");
+		scanf("%d", &mobilidadeescolhida);
+		system("cls");
+
+		printf("Voce alugou a moblidade de ID: %d\n", mobilidadeescolhida);
+		EscolhasClientes();
+		break;
+	case 2:
+		system("cls");
+		printf("Seu saldo atual e: %.2f euros\n", pessoas->saldo);
+		printf("Quanto voce quer adicionar?\n");
+		scanf("%f", &novosaldo);
+		pessoas = AlterarSaldo(pessoas, auxcod,novosaldo);
+		system("cls");
+		EscolhasClientes();
+		break;
+	case 3:
+	case 4:
+		SalvarClientesFILE(pessoas);
+		break;
+	default:
+		system("cls");
+		printf("Warning! : Escolha um numero valido\n");
+		EscolhasClientes();
+		break;
+	}
 }
   
 void MenuCliente() {
@@ -20,7 +71,7 @@ void MenuCliente() {
 	printf("1- Entrar\n");
 	printf("2- Cadastre-se\n");
 	printf("3- EXIT\n");
-	printf("4- Ver Lista de Clientes");
+	/*printf("4- Ver Lista de Clientes");*/
 
 	scanf_s("%d", &opcao);
 
@@ -77,7 +128,7 @@ void MenuCliente() {
 		break;
 
 	case 4:
-		ListarClientes(pessoas);
+		/*ListarClientes(pessoas);*/
 		//SalvarClientesFILE(pessoas);
 		break;
 	default:
